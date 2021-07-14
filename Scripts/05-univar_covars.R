9182fe45f9d92cbb1d22bb6f55fca2ce5f81fb08
adawnir <rinwada0310@gmail.com>
2021-07-14 12:57:03 +0100

    Add geographical analysis and fix error with for loop

--- Scripts/05-univar_covars.R
+++ Scripts/05-univar_covars.R
@@ -31,8 +31,8 @@ for (i in 1:length(batches)){
   betas = pvals = NULL
   f1='expo[,k] ~ X[,j]'
   t0=Sys.time()
-  for (k in 1:ncol(expo)){
-    for (j in 1:ncol(X)){
+  for (j in 1:ncol(X)){
+    for (k in 1:ncol(expo)){
       model1=lm(as.formula(f1))
       betas=c(betas, coefficients(model1)[2])
       pvals=c(pvals, summary(model1)$coefficients[2,4])
@@ -48,8 +48,8 @@ for (i in 1:length(batches)){
   annot_sub = annot[rownames(betas)]
   
   ## Volcano plot
-  pdf(paste0("../Figures/",filepaths[i],"/Univariate_exposure_covariate.pdf"), width = 10, height = 5*ncol(betas)/2)
-  par(mar=c(5,5,1,1), mfrow = c(ncol(betas)/2,2))
+  pdf(paste0("../Figures/",filepaths[i],"/Univariate_exposure_covariate.pdf"), width = 10, height = 5*ceiling(ncol(betas)/2))
+  par(mar=c(5,5,1,1), mfrow = c(ceiling(ncol(betas)/2),2))
   for (n in 1:ncol(betas)){
     plot(betas[,n], -log10(pvals[,n]), pch=19,
          col=ifelse(pvals[,n] < 0.05/length(betas[,n]), annot.colours[annot_sub], "grey"),
@@ -69,8 +69,105 @@ for (i in 1:length(batches)){
   ifelse(dir.exists(paste0("../Results/",filepaths[i])),"",dir.create(paste0("../Results/",filepaths[i])))
   saveRDS(pvals, paste0("../Results/",filepaths[i],"/Univariate_exposure_covariate_pvals.rds"))
   saveRDS(betas, paste0("../Results/",filepaths[i],"/Univariate_exposure_covariate_betas.rds"))
+  
+  # Pooled analysis: by Batch, Region and Department
+  pvals = NULL
+  if (i %in% 4:5){
+    if (i==4){X = covars %>% select(Batch, Region, Department)}
+    if (i==5){X = covars %>% select(Batch)}
+    ncol(expo) * ncol(X) # number of tests
+    betas = pvals = NULL
+    f1='expo[,k] ~ X[,j]'
+    f0='expo[,k] ~ 1'
+    t0=Sys.time()
+    for (j in 1:ncol(X)){
+      for (k in 1:ncol(expo)){
+        model1=lm(as.formula(f1))
+        model0=lm(as.formula(f0))
+        pvals=c(pvals, anova(model0, model1, test = 'Chisq')$`Pr(>Chi)`[2])
+      }
+    }
+    t1=Sys.time()
+    print(t1-t0)
+    pvals = ifelse(pvals ==0, .Machine$double.xmin, pvals)
+    pvals = matrix(pvals, nrow = ncol(expo), ncol = ncol(X))
+    rownames(pvals)=colnames(expo)
+    colnames(pvals)=colnames(X)
+    # If p-value = 0 replace with smallest double in R
+    saveRDS(pvals, paste0("../Results/",filepaths[i],"/Univariate_exposure_geo_pvals.rds"))
+    assign(paste0("pvals_",suffix[i]),pvals)
+  }
 }
 
+annot_sub = annot[rownames(pvals_pooled3)]
+
+# Manhattan plot
+xseq = seq(1, nrow(pvals_pooled3))
+
+for (j in colnames(pvals_pooled3)){
+  {pdf(paste0("../Figures/",filepaths[4],"/Univariate_exposure_",j,".pdf"), width=14, height=8)
+    par(mar=c(20,5,1,1))
+    plot(-log10(pvals_pooled3[,j]),
+         pch = 17, col=annot.colours[annot_sub],
+         xaxt="n", xlab="", ylab = expression(-log[10](italic(p))), cex.lab=1.5,
+         ylim = c(min(-log10(pvals_pooled3[,j]), na.rm = TRUE), max(-log10(pvals_pooled3[,j]), na.rm = TRUE)))
+    abline(h = -log10(0.05), lty = 2, col = "grey")
+    abline(v = xseq, lty = 3, col = "grey", lwd = 0.7)
+    abline(h = -log10(0.05/nrow(pvals_pooled3)), lty = 2, col = darken(batch.colours[4], 0.5))
+    for(i in 1:length(xseq)){
+      axis(1, at=xseq[i], labels = rownames(pvals_pooled3)[i], las=2, cex.axis = 0.8,
+           col.axis = ifelse(isTRUE(pvals_pooled3[i,j] < 0.05/nrow(pvals_pooled3)),
+                             darken(annot.colours[(annot_sub)[i]], amount=0.5),"black"))
+    }
+    xgroup=c(which(!duplicated(annot_sub))-0.5, length(rownames(pvals_pooled3))+0.5)
+    axis(side=1, line=8, at=xgroup, labels=NA)
+    tmp=apply(rbind(xgroup[-length(xgroup)],xgroup[-1]),2,mean)
+    for (k in 1:length(unique(annot_sub))){
+      axis(side=1, line=8, at=tmp[k], labels=unique(annot_sub)[k], tick=FALSE, las=2, 
+           col.axis=darken(annot.colours[unique(annot_sub)[k]], amount=0.5), cex.axis = 0.9)
+    }
+    legend("topleft", lty = c(rep(2,2)),
+           col=c(darken(batch.colours[4], 0.5), "grey"),
+           legend = c("Bonferroni threshold","Nominal threshold"),
+           bg="white", cex = 0.7)
+    dev.off()
+  }
+}
+
+annot_sub = annot[rownames(pvals_pooled2)]
+
+# Manhattan plot
+xseq = seq(1, nrow(pvals_pooled2))
+
+for (j in colnames(pvals_pooled2)){
+  {pdf(paste0("../Figures/",filepaths[5],"/Univariate_exposure_",j,".pdf"), width=14, height=8)
+    par(mar=c(20,5,1,1))
+    plot(-log10(pvals_pooled2[,j]),
+         pch = 17, col=annot.colours[annot_sub],
+         xaxt="n", xlab="", ylab = expression(-log[10](italic(p))), cex.lab=1.5,
+         ylim = c(min(-log10(pvals_pooled2[,j]), na.rm = TRUE), max(-log10(pvals_pooled2[,j]), na.rm = TRUE)))
+    abline(h = -log10(0.05), lty = 2, col = "grey")
+    abline(v = xseq, lty = 3, col = "grey", lwd = 0.7)
+    abline(h = -log10(0.05/nrow(pvals_pooled2)), lty = 2, col = darken(batch.colours[5], 0.5))
+    for(i in 1:length(xseq)){
+      axis(1, at=xseq[i], labels = rownames(pvals_pooled2)[i], las=2, cex.axis = 0.8,
+           col.axis = ifelse(isTRUE(pvals_pooled2[i,j] < 0.05/nrow(pvals_pooled2)),
+                             darken(annot.colours[(annot_sub)[i]], amount=0.5),"black"))
+    }
+    xgroup=c(which(!duplicated(annot_sub))-0.5, length(rownames(pvals_pooled2))+0.5)
+    axis(side=1, line=8, at=xgroup, labels=NA)
+    tmp=apply(rbind(xgroup[-length(xgroup)],xgroup[-1]),2,mean)
+    for (k in 1:length(unique(annot_sub))){
+      axis(side=1, line=8, at=tmp[k], labels=unique(annot_sub)[k], tick=FALSE, las=2, 
+           col.axis=darken(annot.colours[unique(annot_sub)[k]], amount=0.5), cex.axis = 0.9)
+    }
+    legend("topleft", lty = c(rep(2,2)),
+           col=c(darken(batch.colours[5], 0.5), "grey"),
+           legend = c("Bonferroni threshold","Nominal threshold"),
+           bg="white", cex = 0.7)
+    dev.off()
+  }
+}
 
 ### Detection ~ Covariate ----
 # Initialisation
@@ -105,8 +202,8 @@ for (i in 1:length(batches)){
   betas = pvals = NULL
   f1='expo[,k] ~ X[,j]'
   t0=Sys.time()
-  for (k in 1:ncol(expo)){
-    for (j in 1:ncol(X)){
+  for (j in 1:ncol(X)){
+    for (k in 1:ncol(expo)){
       model1=glm(as.formula(f1), family = "binomial")
       betas=c(betas, coefficients(model1)[2])
       pvals=c(pvals, summary(model1)$coefficients[2,4])
@@ -122,8 +219,8 @@ for (i in 1:length(batches)){
   annot_sub = annot[rownames(betas)]
   
   ## Volcano plot
-  pdf(paste0("../Figures/",filepaths[i],"/Univariate_detection_covariate.pdf"), width = 10, height = 5*ncol(betas)/2)
-  par(mar=c(5,5,1,1), mfrow = c(ncol(betas)/2,2))
+  pdf(paste0("../Figures/",filepaths[i],"/Univariate_detection_covariate.pdf"), width = 10, height = 5*ceiling(ncol(betas)/2))
+  par(mar=c(5,5,1,1), mfrow = c(ceiling(ncol(betas)/2),2))
   for (n in 1:ncol(betas)){
     plot(betas[,n], -log10(pvals[,n]), pch=19,
          col=ifelse(pvals[,n] < 0.05/length(betas[,n]), annot.colours[annot_sub], "grey"),
@@ -132,7 +229,7 @@ for (i in 1:length(batches)){
          xlim = c(-max(abs(betas[,n])), max(abs(betas[,n]))),
          ylab=expression(-log[10](p-value)), 
          xlab=substitute(paste(beta,"(",tmp,")"), list(tmp = colnames(betas)[n])))
-    text(betas[,n]+sign(betas[,n])*5, -log10(pvals[,n])+0.25,
+    text(betas[,n]+sign(betas[,n])*0.5, -log10(pvals[,n])+0.25,
          labels = ifelse(pvals[,n] < 0.05/length(betas[,n]), rownames(betas), ""),
          col = annot.colours[annot_sub])
     abline(h = -log10(0.05/length(betas[,n])), lty = 2)
@@ -140,6 +237,102 @@ for (i in 1:length(batches)){
   dev.off()
   saveRDS(pvals, paste0("../Results/",filepaths[i],"/Univariate_detection_covariate_pvals.rds"))
   saveRDS(betas, paste0("../Results/",filepaths[i],"/Univariate_detection_covariate_betas.rds"))
-  # assign(paste0("betas_",suffix[i]),betas)
-  # assign(paste0("pvals_",suffix[i]),pvals)
+  
+  # Pooled analysis: by Batch, Region and Department
+  pvals = NULL
+  if (i %in% 4:5){
+    if (i==4){X = covars %>% select(Batch, Region, Department)}
+    if (i==5){X = covars %>% select(Batch)}
+    ncol(expo) * ncol(X) # number of tests
+    betas = pvals = NULL
+    f1='expo[,k] ~ X[,j]'
+    f0='expo[,k] ~ 1'
+    t0=Sys.time()
+    for (j in 1:ncol(X)){
+      for (k in 1:ncol(expo)){
+        model1=lm(as.formula(f1))
+        model0=lm(as.formula(f0))
+        pvals=c(pvals, anova(model0, model1, test = 'Chisq')$`Pr(>Chi)`[2])
+      }
+    }
+    t1=Sys.time()
+    print(t1-t0)
+    pvals = ifelse(pvals ==0, .Machine$double.xmin, pvals)
+    pvals = matrix(pvals, nrow = ncol(expo), ncol = ncol(X))
+    rownames(pvals)=colnames(expo)
+    colnames(pvals)=colnames(X)
+    # If p-value = 0 replace with smallest double in R
+    saveRDS(pvals, paste0("../Results/",filepaths[i],"/Univariate_detection_geo_pvals.rds"))
+    assign(paste0("pvals_",suffix[i]),pvals)
+  }
+}
+
+annot_sub = annot[rownames(pvals_pooled3)]
+
+# Manhattan plot
+xseq = seq(1, nrow(pvals_pooled3))
+
+for (j in colnames(pvals_pooled3)){
+  {pdf(paste0("../Figures/",filepaths[4],"/Univariate_detection_",j,".pdf"), width=14, height=8)
+    par(mar=c(20,5,1,1))
+    plot(-log10(pvals_pooled3[,j]),
+         pch = 17, col=annot.colours[annot_sub],
+         xaxt="n", xlab="", ylab = expression(-log[10](italic(p))), cex.lab=1.5,
+         ylim = c(min(-log10(pvals_pooled3[,j]), na.rm = TRUE), max(-log10(pvals_pooled3[,j]), na.rm = TRUE)))
+    abline(h = -log10(0.05), lty = 2, col = "grey")
+    abline(v = xseq, lty = 3, col = "grey", lwd = 0.7)
+    abline(h = -log10(0.05/nrow(pvals_pooled3)), lty = 2, col = darken(batch.colours[4], 0.5))
+    for(i in 1:length(xseq)){
+      axis(1, at=xseq[i], labels = rownames(pvals_pooled3)[i], las=2, cex.axis = 0.7,
+           col.axis = ifelse(isTRUE(pvals_pooled3[i,j] < 0.05/nrow(pvals_pooled3)),
+                             darken(annot.colours[(annot_sub)[i]], amount=0.5),"black"))
+    }
+    xgroup=c(which(!duplicated(annot_sub))-0.5, length(rownames(pvals_pooled3))+0.5)
+    axis(side=1, line=8, at=xgroup, labels=NA)
+    tmp=apply(rbind(xgroup[-length(xgroup)],xgroup[-1]),2,mean)
+    for (k in 1:length(unique(annot_sub))){
+      axis(side=1, line=8, at=tmp[k], labels=unique(annot_sub)[k], tick=FALSE, las=2, 
+           col.axis=darken(annot.colours[unique(annot_sub)[k]], amount=0.5), cex.axis = 0.9)
+    }
+    legend("topleft", lty = c(rep(2,2)),
+           col=c(darken(batch.colours[4], 0.5), "grey"),
+           legend = c("Bonferroni threshold","Nominal threshold"),
+           bg="white", cex = 0.7)
+    dev.off()
+  }
+}
+
+annot_sub = annot[rownames(pvals_pooled2)]
+
+# Manhattan plot
+xseq = seq(1, nrow(pvals_pooled2))
+
+for (j in colnames(pvals_pooled2)){
+  {pdf(paste0("../Figures/",filepaths[5],"/Univariate_detection_",j,".pdf"), width=14, height=8)
+    par(mar=c(20,5,1,1))
+    plot(-log10(pvals_pooled2[,j]),
+         pch = 17, col=annot.colours[annot_sub],
+         xaxt="n", xlab="", ylab = expression(-log[10](italic(p))), cex.lab=1.5,
+         ylim = c(min(-log10(pvals_pooled2[,j]), na.rm = TRUE), max(-log10(pvals_pooled2[,j]), na.rm = TRUE)))
+    abline(h = -log10(0.05), lty = 2, col = "grey")
+    abline(v = xseq, lty = 3, col = "grey", lwd = 0.7)
+    abline(h = -log10(0.05/nrow(pvals_pooled2)), lty = 2, col = darken(batch.colours[5], 0.5))
+    for(i in 1:length(xseq)){
+      axis(1, at=xseq[i], labels = rownames(pvals_pooled2)[i], las=2, cex.axis = 0.7,
+           col.axis = ifelse(isTRUE(pvals_pooled2[i,j] < 0.05/nrow(pvals_pooled2)),
+                             darken(annot.colours[(annot_sub)[i]], amount=0.5),"black"))
+    }
+    xgroup=c(which(!duplicated(annot_sub))-0.5, length(rownames(pvals_pooled2))+0.5)
+    axis(side=1, line=8, at=xgroup, labels=NA)
+    tmp=apply(rbind(xgroup[-length(xgroup)],xgroup[-1]),2,mean)
+    for (k in 1:length(unique(annot_sub))){
+      axis(side=1, line=8, at=tmp[k], labels=unique(annot_sub)[k], tick=FALSE, las=2, 
+           col.axis=darken(annot.colours[unique(annot_sub)[k]], amount=0.5), cex.axis = 0.9)
+    }
+    legend("topleft", lty = c(rep(2,2)),
+           col=c(darken(batch.colours[5], 0.5), "grey"),
+           legend = c("Bonferroni threshold","Nominal threshold"),
+           bg="white", cex = 0.7)
+    dev.off()
+  }
 }
