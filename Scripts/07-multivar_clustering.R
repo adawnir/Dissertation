4977bcf3b0cba0e5c405348d0866365c2e38db68
adawnir <rinwada0310@gmail.com>
2021-07-21 19:44:01 +0100

    Family colours; Changing labels; Allowing gender NA

--- Scripts/07-multivar_clustering.R
+++ Scripts/07-multivar_clustering.R
@@ -5,6 +5,7 @@
 library(ape)
 library(igraph)
 
+### Family ----
 # Initialise
 rm(list=ls())
 path=dirname(rstudioapi::getActiveDocumentContext()$path)
@@ -26,10 +27,6 @@ for (i in 1:length(batches)){
   
   families=unique(covars$Family.ID)
   
-  mycolours=brewer.pal(n=12,name='Paired')
-  mycolours=colorRampPalette(mycolours)(length(families))
-  names(mycolours)=families
-  
   expo = scale(expo)
   d=dist(expo)
   h=hclust(d, method = "complete")
@@ -40,12 +37,12 @@ for (i in 1:length(batches)){
   {pdf(paste0("../Figures/",filepaths[i],"/Hierarchical_cont_expo_all.pdf"),width=14)
     par(mar=c(0,0,0,0))
     plot(myphylo, direction="downwards", cex=0.5, #srt=180, adj=1,
-         tip.color=mycolours[as.character(covars$Family.ID)])
+         tip.color=family.colours[as.character(covars$Family.ID)])
     dev.off()
   }
   
   {pdf(paste0("../Figures/",filepaths[i],"/Hierarchical_cont_graph_expo_all.pdf"))
-    g=ClusteringToGraph(covars=covars, myphylo=myphylo)
+    g=ClusteringToGraph(covars=covars, myphylo=myphylo, mycol = family.colours[levels(covars$Family.ID)])
     dev.off()
   }
   
@@ -62,12 +59,13 @@ for (i in 1:length(batches)){
   
   {pdf(paste0("../Figures/",filepaths[i],"/Shortest_path_cont_all.pdf"),width=14)
     par(mar=c(5,5,1,1))
-    plot(fsp, pch=19, col=mycolours[families], xaxt="n", las=1,
+    plot(fsp, pch=19, col=family.colours[families], xaxt="n", las=1,
          panel.first=abline(v=1:length(families), lty=3, col="grey"),
          xlab="Family ID", cex=2,
          ylab="Shortest path length between siblings", cex.lab=1.5)
     axis(side=1, at=1:length(families), labels=families, las = 2)
-    dev.off()}
+    dev.off()
+    }
   
   ## Relationship with age
   age_mu=rep(NA, length(families))
@@ -79,9 +77,9 @@ for (i in 1:length(batches)){
   model=lm(fsp~age_mu)
   {pdf(paste0("../Figures/",filepaths[i],"/Shortest_path_cont_all_vs_age_mean.pdf"))
     par(mar=c(5,5,1,1))
-    plot(age_mu, fsp, pch=19, cex=1, cex.lab=1.5, las=1, col=mycolours[names(age_mu)],
+    plot(age_mu, fsp, pch=19, cex=1, cex.lab=1.5, las=1, col=family.colours[names(age_mu)],
          xlab="Within-family age mean", ylab="Shortest path length between siblings")
-    text(age_mu, fsp, labels=names(age_mu), pos=3, col=darken(mycolours[names(age_mu)], amount=0.5))
+    text(age_mu, fsp, labels=names(age_mu), pos=3, col=darken(family.colours[names(age_mu)], amount=0.5))
     abline(h=axTicks(2), col="grey", lty=3)
     abline(v=axTicks(1), col="grey", lty=3)
     legend("topright", bty="n", cex=1.5,
@@ -98,9 +96,9 @@ for (i in 1:length(batches)){
   model=lm(fsp~age_diff)
   {pdf(paste0("../Figures/",filepaths[i],"/Shortest_path_cont_all_vs_age_diff.pdf"))
     par(mar=c(5,5,1,1))
-    plot(age_diff, fsp, pch=19, cex=1, cex.lab=1.5, las=1, col=mycolours[names(age_diff)],
+    plot(age_diff, fsp, pch=19, cex=1, cex.lab=1.5, las=1, col=family.colours[names(age_diff)],
          xlab="Within-family age absolute difference", ylab="Shortest path length between siblings")
-    text(age_diff, fsp, labels=names(age_diff), pos=3, col=darken(mycolours[names(age_diff)], amount=0.5))
+    text(age_diff, fsp, labels=names(age_diff), pos=3, col=darken(family.colours[names(age_diff)], amount=0.5))
     abline(h=axTicks(2), col="grey", lty=3)
     abline(v=axTicks(1), col="grey", lty=3)
     legend("topright", bty="n", cex=1.5,
@@ -118,9 +116,9 @@ for (i in 1:length(batches)){
     model=lm(fsp~weight_mu)
     {pdf(paste0("../Figures/",filepaths[i],"/Shortest_path_cont_all_vs_weight_mean.pdf"))
       par(mar=c(5,5,1,1))
-      plot(weight_mu, fsp, pch=19, cex=1, cex.lab=1.5, las=1, col=mycolours[names(weight_mu)],
+      plot(weight_mu, fsp, pch=19, cex=1, cex.lab=1.5, las=1, col=family.colours[names(weight_mu)],
            xlab="Within-family weight mean", ylab="Shortest path length between siblings")
-      text(weight_mu, fsp, labels=names(weight_mu), pos=3, col=darken(mycolours[names(weight_mu)], amount=0.5))
+      text(weight_mu, fsp, labels=names(weight_mu), pos=3, col=darken(family.colours[names(weight_mu)], amount=0.5))
       abline(h=axTicks(2), col="grey", lty=3)
       abline(v=axTicks(1), col="grey", lty=3)
       legend("topleft", bty="n", cex=1.5,
@@ -137,9 +135,9 @@ for (i in 1:length(batches)){
     model=lm(fsp~weight_diff)
     {pdf(paste0("../Figures/",filepaths[i],"/Shortest_path_cont_all_vs_weight_diff.pdf"))
       par(mar=c(5,5,1,1))
-      plot(weight_diff, fsp, pch=19, cex=1, cex.lab=1.5, las=1, col=mycolours[names(weight_diff)],
+      plot(weight_diff, fsp, pch=19, cex=1, cex.lab=1.5, las=1, col=family.colours[names(weight_diff)],
            xlab="Within-family weight absolute difference", ylab="Shortest path length between siblings")
-      text(weight_diff, fsp, labels=names(weight_diff), pos=3, col=darken(mycolours[names(weight_diff)], amount=0.5))
+      text(weight_diff, fsp, labels=names(weight_diff), pos=3, col=darken(family.colours[names(weight_diff)], amount=0.5))
       abline(h=axTicks(2), col="grey", lty=3)
       abline(v=axTicks(1), col="grey", lty=3)
       legend("topright", bty="n", cex=1.5,
@@ -156,9 +154,9 @@ for (i in 1:length(batches)){
     model=lm(fsp~length_mu)
     {pdf(paste0("../Figures/",filepaths[i],"/Shortest_path_cont_all_vs_length_mean.pdf"))
       par(mar=c(5,5,1,1))
-      plot(length_mu, fsp, pch=19, cex=1, cex.lab=1.5, las=1, col=mycolours[names(length_mu)],
+      plot(length_mu, fsp, pch=19, cex=1, cex.lab=1.5, las=1, col=family.colours[names(length_mu)],
            xlab="Within-family length mean", ylab="Shortest path length between siblings")
-      text(length_mu, fsp, labels=names(length_mu), pos=3, col=darken(mycolours[names(length_mu)], amount=0.5))
+      text(length_mu, fsp, labels=names(length_mu), pos=3, col=darken(family.colours[names(length_mu)], amount=0.5))
       abline(h=axTicks(2), col="grey", lty=3)
       abline(v=axTicks(1), col="grey", lty=3)
       legend("topright", bty="n", cex=1.5,
@@ -175,9 +173,9 @@ for (i in 1:length(batches)){
     model=lm(fsp~length_diff)
     {pdf(paste0("../Figures/",filepaths[i],"/Shortest_path_cont_all_vs_length_diff.pdf"))
       par(mar=c(5,5,1,1))
-      plot(length_diff, fsp, pch=19, cex=1, cex.lab=1.5, las=1, col=mycolours[names(length_diff)],
+      plot(length_diff, fsp, pch=19, cex=1, cex.lab=1.5, las=1, col=family.colours[names(length_diff)],
            xlab="Within-family length absolute difference", ylab="Shortest path length between siblings")
-      text(length_diff, fsp, labels=names(length_diff), pos=3, col=darken(mycolours[names(length_diff)], amount=0.5))
+      text(length_diff, fsp, labels=names(length_diff), pos=3, col=darken(family.colours[names(length_diff)], amount=0.5))
       abline(h=axTicks(2), col="grey", lty=3)
       abline(v=axTicks(1), col="grey", lty=3)
       legend("topright", bty="n", cex=1.5,
@@ -189,7 +187,9 @@ for (i in 1:length(batches)){
   gender_diff=rep(NA, length(families))
   for (f in 1:length(families)){
     tmp=covars$Gender[covars$Family.ID==families[f]]
-    if (length(unique(tmp))==1){
+    if (sum(is.na(tmp))>0) {
+      gender_diff[f] = NA
+    } else if (length(unique(tmp))==1){
       if (unique(tmp)=="Male"){
         gender_diff[f]=1
       }
@@ -198,18 +198,18 @@ for (i in 1:length(batches)){
       }
     } else {
       gender_diff[f]=3
-    } 
+    }
   }
   names(gender_diff)=families
   
-  model1 = lm(fsp ~ as.factor(gender_diff))
-  model0 = lm(fsp ~ 1)
+  model1 = lm(fsp[!is.na(gender_diff)] ~ as.factor(gender_diff[!is.na(gender_diff)]))
+  model0 = lm(fsp[!is.na(gender_diff)] ~ 1)
   {pdf(paste0("../Figures/",filepaths[i],"/Shortest_path_cont_all_by_gender.pdf"))
     par(mar=c(5,5,1,1))
-    plot(fsp, pch=19, cex=1, cex.lab=1.5, las=1, 
-         col=c("skyblue", "pink", "tan")[gender_diff],
-         xlab="Family ID", ylab="Shortest path length between siblings")
-    text(fsp, labels=names(gender_diff), pos=3, col=darken(mycolours[names(gender_diff)], amount=0.5))
+    plot(fsp[!is.na(gender_diff)], pch=19, cex=1, cex.lab=1.5, las=1, 
+         col=c("skyblue", "pink", "tan")[gender_diff[!is.na(gender_diff)]],
+         xlab="Family", ylab="Shortest path length between siblings")
+    text(fsp[!is.na(gender_diff)], labels=names(gender_diff)[!is.na(gender_diff)], pos=3, col=darken(family.colours[names(gender_diff[!is.na(gender_diff)])], amount=0.5))
     legend("topleft", pch=19, col=c("skyblue", "pink", "tan"), ncol=1,
            legend=c("All Male", "All Female", "Different genders"))
     legend("topright", bty="n", cex=1.5,
@@ -232,8 +232,8 @@ for (i in 1:length(batches)){
       par(mar=c(5,5,1,1))
       plot(fsp, pch=19, cex=1, cex.lab=1.5, las=1, 
            col=batch.colours[Batch],
-           xlab="Family ID", ylab="Shortest path length between siblings")
-      text(fsp, labels=names(Batch), pos=3, col=darken(mycolours[names(Batch)], amount=0.5))
+           xlab="Family", ylab="Shortest path length between siblings")
+      text(fsp, labels=names(Batch), pos=3, col=darken(family.colours[names(Batch)], amount=0.5))
       legend("topleft", pch=19, col=batch.colours[unique(Batch)],
              legend=levels(covars$Batch))
       legend("topright", bty="n", cex=1.5,
@@ -257,22 +257,14 @@ for (i in 1:length(batches)){
     names(Region)=families
     names(Department)=families
     
-    region.colours=brewer.pal(n=12,name='Paired')
-    region.colours=colorRampPalette(region.colours)(length(levels(covars$Region)))
-    names(region.colours)=levels(covars$Region)
-    
-    depart.colours=brewer.pal(n=12,name='Paired')
-    depart.colours=colorRampPalette(depart.colours)(length(levels(covars$Department)))
-    names(depart.colours)=levels(covars$Department)
-    
     model1 = lm(fsp ~ as.factor(Region))
     model0 = lm(fsp ~ 1)
     {pdf(paste0("../Figures/",filepaths[i],"/Shortest_path_cont_all_by_Region.pdf"))
       par(mar=c(5,5,1,1))
       plot(fsp, pch=19, cex=1, cex.lab=1.5, las=1, 
            col=region.colours[Region],
-           xlab="Family ID", ylab="Shortest path length between siblings")
-      text(fsp, labels=names(Region), pos=3, col=darken(mycolours[names(Region)], amount=0.5))
+           xlab="Family", ylab="Shortest path length between siblings")
+      text(fsp, labels=names(Region), pos=3, col=darken(family.colours[names(Region)], amount=0.5))
       legend("topleft", pch=19, col=region.colours[unique(Region)],
              legend=levels(covars$Region), cex = 0.6, ncol = 2)
       legend("topright", bty="n", cex=1.5,
@@ -286,8 +278,8 @@ for (i in 1:length(batches)){
       par(mar=c(5,5,1,1))
       plot(fsp, pch=19, cex=1, cex.lab=1.5, las=1, 
            col=depart.colours[Department],
-           xlab="Family ID", ylab="Shortest path length between siblings")
-      text(fsp, labels=names(Department), pos=3, col=darken(mycolours[names(Department)], amount=0.5))
+           xlab="Family", ylab="Shortest path length between siblings")
+      text(fsp, labels=names(Department), pos=3, col=darken(family.colours[names(Department)], amount=0.5))
       legend("topleft", pch=19, col=depart.colours[unique(Department)],
              legend=levels(covars$Department), cex = 0.6, ncol = 2)
       legend("topright", bty="n", cex=1.5,
@@ -296,6 +288,7 @@ for (i in 1:length(batches)){
     }
   }
 }
+
 ### Geographical ----
 for (i in 4:5){
   # Load data
