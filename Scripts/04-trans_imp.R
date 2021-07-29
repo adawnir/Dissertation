f740369ea02d8bb727e02a193ae3c4a2d637c284
adawnir <rinwada0310@gmail.com>
2021-07-29 15:11:52 +0100

    Undo

--- Scripts/04-trans_imp.R
+++ Scripts/04-trans_imp.R
@@ -13,7 +13,7 @@ setwd(path)
 # Load custom parameters
 source("graph_param.R")
 
-for (i in 1:3){
+for (i in 1:length(batches)){
   # Load data
   covars = readRDS(paste0("../Processed/",filepaths[i],"/Participant_covariate_info_thresh.rds"))
   expo = readRDS(paste0("../Processed/",filepaths[i],"/Exposure_matrix_ndimp_thresh.rds"))
@@ -34,29 +34,29 @@ for (i in 1:3){
   saveRDS(covars, paste0("../Processed/",filepaths[i],"/Participant_covariate_info_thresh_naprop.rds"))
 }
 
-for (i in 4:5){
-  # Load data
-  covars = readRDS(paste0("../Processed/",filepaths[i],"/Participant_covariate_info_thresh.rds"))
-  expo = readRDS(paste0("../Processed/",filepaths[i],"/Exposure_matrix_ndimp_thresh.rds"))
-  # Transformation
-  expo = log10(expo)
-  saveRDS(expo, paste0("../Processed/",filepaths[i],"/Exposure_matrix_ndimp_thresh_log.rds"))
-  # Missing rate per participant
-  covars$NA_prop = apply(expo, 1, function(x) sum(is.na(x))/ncol(expo))
-  print(max(covars$NA_prop) < 0.8)
-  
-  # impute.QRILC
-  expo_imp = NULL
-  for (k in levels(covars$Batch)){
-    set.seed(7)
-    tmp = impute.QRILC(t(expo[covars$Batch == k,]))
-    tmp=t(tmp[[1]])
-    expo_imp = rbind(expo_imp, tmp)
-  }
-  all(rownames(expo_imp) == rownames(expo))
-  all(colnames(expo_imp) == colnames(expo))
-  
-  # Save data sets
-  saveRDS(expo_imp, paste0("../Processed/",filepaths[i],"/Exposure_matrix_ndimp_thresh_log_naimp.rds"))
-  saveRDS(covars, paste0("../Processed/",filepaths[i],"/Participant_covariate_info_thresh_naprop.rds"))
-}
+# for (i in 4:5){
+#   # Load data
+#   covars = readRDS(paste0("../Processed/",filepaths[i],"/Participant_covariate_info_thresh.rds"))
+#   expo = readRDS(paste0("../Processed/",filepaths[i],"/Exposure_matrix_ndimp_thresh.rds"))
+#   # Transformation
+#   expo = log10(expo)
+#   saveRDS(expo, paste0("../Processed/",filepaths[i],"/Exposure_matrix_ndimp_thresh_log.rds"))
+#   # Missing rate per participant
+#   covars$NA_prop = apply(expo, 1, function(x) sum(is.na(x))/ncol(expo))
+#   print(max(covars$NA_prop) < 0.8)
+#   
+#   # impute.QRILC
+#   expo_imp = NULL
+#   for (k in levels(covars$Batch)){
+#     set.seed(7)
+#     tmp = impute.QRILC(t(expo[covars$Batch == k,]))
+#     tmp=t(tmp[[1]])
+#     expo_imp = rbind(expo_imp, tmp)
+#   }
+#   all(rownames(expo_imp) == rownames(expo))
+#   all(colnames(expo_imp) == colnames(expo))
+#   
+#   # Save data sets
+#   saveRDS(expo_imp, paste0("../Processed/",filepaths[i],"/Exposure_matrix_ndimp_thresh_log_naimp.rds"))
+#   saveRDS(covars, paste0("../Processed/",filepaths[i],"/Participant_covariate_info_thresh_naprop.rds"))
+# }
