fc036ca7231813aa8345581640df559faf2395c5
adawnir <rinwada0310@gmail.com>
2021-08-09 12:04:13 +0100

    Adjust text

--- Scripts/08-shortest_path_association.R
+++ Scripts/08-shortest_path_association.R
@@ -24,54 +24,84 @@ annot = readRDS("../Data/Chemical_compound_family_annotation.rds")
 
 suffix = c("lux","fra","gs","pooled3","pooled2")
 
+X = readRDS(paste0("../Results/",filepaths[m],"/Family_covariates_delta_exposures.rds"))
+fsp = readRDS(paste0("../Results/",filepaths[m],"/Shortest_path_cont_all.rds"))
+
 betas = pvals = NULL
-f1='fsp ~ delta_mat[,k]'
+f1='Y ~ X[,k]'
 t0=Sys.time()
-for (k in 1:ncol(delta_mat)){
+for (k in 1:ncol(X)){
+  Y = fsp
   model1=lm(as.formula(f1))
-  betas=c(betas, coefficients(model1)[2])
-  pvals=c(pvals, summary(model1)$coefficients[2,4])
+  if(colnames(X)[k] == "gender_diff"){
+    betas=c(betas, coefficients(model1)[2:length(coefficients(model1))])
+    pvals=c(pvals, summary(model1)$coefficients[2:nrow(summary(model1)$coefficients),4])
+  } else {
+    betas=c(betas, coefficients(model1)[2])
+    pvals=c(pvals, summary(model1)$coefficients[2,4])
+  }
 }
 t1=Sys.time()
 print(t1-t0)
-names(pvals)=names(betas)=colnames(delta_mat)
 
-{pdf(paste0("../Figures/",filepaths[m],"/Shortest_path_vs_delta_expo.pdf"))
+mylabels = c(colnames(X)[1:which(colnames(X)=="gender_diff")-1],
+             "All male (ref. All female)","Different gender (ref. All female)",
+             colnames(X)[(which(colnames(X)=="gender_diff")+1):ncol(X)])
+
+length(betas)==length(mylabels)
+
+mylabels = gsub("age_mu","Mean age",mylabels)
+mylabels = gsub("age_diff","Age difference",mylabels)
+mylabels = gsub("length_mu","Mean sample length",mylabels)
+mylabels = gsub("length_diff","Sample length difference",mylabels)
+mylabels = gsub("weight_mu","Mean sample weight",mylabels)
+mylabels = gsub("weight_diff","Sample weight difference",mylabels)
+mylabels = gsub("Region","Île-de-France (Y/N)",mylabels)
+
+names(pvals)=names(betas)=mylabels
+
+saveRDS(pvals, paste0("../Results/",filepaths[m],"/Shortest_path_univar_pvals.rds"))
+saveRDS(betas, paste0("../Results/",filepaths[m],"/Shortest_path_univar_betas.rds"))
+
+annot_sub = annot[which(names(annot) %in% mylabels)]
+
+mycolours = c(rep(batch.colours[m],length(mylabels)-length(annot_sub)),annot.colours[annot_sub])
+
+{pdf(paste0("../Figures/",filepaths[m],"/Shortest_path_univariate.pdf"))
   par(mar=c(5,5,1,1))
   plot(betas, -log10(pvals), pch=19,
-       col=ifelse(pvals < 0.05/length(betas), annot.colours[annot_sub], "grey"),
+       col=ifelse(pvals < 0.05, mycolours, "grey"),
        cex.lab=1, cex = 0.7,
        ylim = c(0, max(-log10(pvals))+0.25),
-       xlim = c(-max(abs(betas))-0.4, max(abs(betas))+0.4),
+       xlim = c(-max(abs(betas))-5, max(abs(betas))+5),
        ylab=expression(-log[10](p)), 
        xlab=expression(beta))
-  text(betas+sign(betas)*0.1, -log10(pvals)+0.25,
-       labels = ifelse(pvals < 0.05/length(betas), names(betas), ""),
-       col = annot.colours[annot_sub])
-  abline(h = -log10(0.05/length(betas)), lty = 2)
+  for (k in 1:length(mylabels)){
+    if(mylabels[k] %in% names(annot_sub)){
+      if(pvals[k] < 0.05){
+        label = substitute(Delta~tmp, list(tmp=mylabels[k]))
+        text(betas[k]+sign(betas[k])*2, -log10(pvals[k])+0.1,
+             labels = label, col = mycolours[k])
+      } else {
+        text(betas[k]+sign(betas[k])*2, -log10(pvals[k])+0.1,
+             labels = mylabels[k], col = mycolours[k]) 
+        }
+    }
+  }
+  text(max(abs(betas))+4.9, -log10(0.05/ncol(X))+0.01, paste0("Bonferroni threshold = ",formatC(-log10(0.05/ncol(X)), format = "e")), adj = c(1,0), col = "darkred")
+  abline(h = -log10(0.05/ncol(X)), lty = 2, col = "darkred")
   dev.off()
 }
 
-### Multivariate analysis using stability selection LASSO regression ----
-if (m == 1){
-  X = cbind(age_mu, age_diff, gender_diff,
-            length_mu, length_diff, weight_mu, weight_diff,
-            as.data.frame(delta_mat))
-}
-if (m %in% c(2,4)){
-  X = cbind(age_mu, age_diff, gender_diff,
-            Region,
-            as.data.frame(delta_mat))
-}
-if (m %in% c(3,5)){
-  X = cbind(age_mu, age_diff, gender_diff,
-            as.data.frame(delta_mat))
-}
+### Multivariate analysis using stability selection sPLS regression ----
 
 Y = fsp[complete.cases(X)]
+X = X[complete.cases(X),]
 X = model.matrix(~., X)[,-1]
 
 stab = VariableSelection(xdata = X, ydata = Y, implementation = SparsePLS)
+
+saveRDS(stab, paste0("../Results/",filepaths[m],"/Shortest_path_univar_stab.rds"))
 
 pdf(paste0("../Figures/",filepaths[m],"/Shortest_path_multivariate_output.pdf"))
 par(mar = c(7, 5, 7, 6))
@@ -120,6 +150,7 @@ plot(selprop_ranked,
      col = ifelse(selprop_ranked >= Argmax(stab)[2], yes = batch.colours[m], no = "grey"),
      xaxt = "n", xlab = "", ylab = "Selection proportions"
 )
+text(length(selprop_ranked)-0.01, Argmax(stab)[2]+0.01, paste0("Selection proportion threshold = ",Argmax(stab)[2]), adj = c(1,0), col = "darkred")
 abline(h = Argmax(stab)[2], lty = 2, col = "darkred")
 axis(side = 1, at = 1:length(selprop_ranked), labels = NA)
 for (k in 1:length(selprop_ranked)){
@@ -130,7 +161,7 @@ for (k in 1:length(selprop_ranked)){
   } else {
     col = "black"
   }
-  if (mylabels[k] %in% colnames(delta_mat)){
+  if (mylabels[k] %in% names(annot_sub)){
     label = substitute(Delta~tmp, list(tmp = mylabels[k]))
   } else {
     label = mylabels[k]
@@ -139,185 +170,3 @@ for (k in 1:length(selprop_ranked)){
        col.axis = ifelse(selprop_ranked[k] >= Argmax(stab)[2], col, "grey"))
 }
 dev.off()
-
-## Only covariates ----
-# This line throws error in sgPLS::sPLS internal function, unless scale = FALSE
-stab = VariableSelection(xdata = X[,1:(ncol(X)-ncol(delta_mat))], ydata = Y,
-                         implementation = SparsePLS, Lambda = 1:(ncol(X) - 1))
-pdf(paste0("../Figures/",filepaths[m],"/Shortest_path_multivariate_covariates_only_output.pdf"))
-par(mar = c(7, 5, 7, 6))
-CalibrationPlot(stab)
-dev.off()
-
-# Checking consistency in sign of the beta coefficients for the variables with high selprop
-# Ideally no point around 0.5 with high selection proportion
-selprop=SelectionProportions(stab)
-a=apply(stab$Beta[ArgmaxId(stab)[1],,],1,FUN=function(x){sum(x>0)})
-a = a[-length(a)]
-b=apply(stab$Beta[ArgmaxId(stab)[1],,],1,FUN=function(x){sum(x<0)})
-b = b[-length(b)]
-pdf(paste0("../Figures/",filepaths[m],"/Shortest_path_multivariate_covariates_only_beta_consistency.pdf"))
-par(mar=c(5,5,1,1))
-plot(a/(a+b), selprop, las=1, pch=19, col="navy", cex.lab=1.5,
-     xlab="Proportion of positive beta among non-zero betas",
-     ylab="Selection Proportion") # Ideally no point around 0.5 with high selection proportion
-dev.off()
-
-# Extract beta
-beta = stab$Beta[ArgmaxId(stab)[1],,]
-beta_mu = rowMeans(beta[-nrow(beta),] %*% diag(beta[nrow(beta),]))
-names(beta_mu) = names(selprop)
-
-selprop_ranked <- sort(selprop, decreasing = TRUE)
-beta_mu_ranked <- beta_mu[order(-selprop)]
-print(all(names(selprop_ranked)==names(beta_mu_ranked)))
-
-mylabels = gsub("age_mu","Mean age",names(selprop_ranked))
-mylabels = gsub("age_diff","Age difference",mylabels)
-mylabels = gsub("gender_diff","",mylabels)
-mylabels = gsub("All male","All male (ref. All female)",mylabels)
-mylabels = gsub("Different genders","Different genders (ref. All female)",mylabels)
-mylabels = gsub("length_mu","Mean sample length",mylabels)
-mylabels = gsub("length_diff","Sample length difference",mylabels)
-mylabels = gsub("weight_mu","Mean sample weight",mylabels)
-mylabels = gsub("weight_diff","Sample weight difference",mylabels)
-mylabels = gsub("Region","Île-de-France (Y/N)",mylabels)
-
-pdf(paste0("../Figures/",filepaths[m],"/Shortest_path_multivariate_covariates_only_selprop.pdf"), width = 14)
-par(mar=c(15, 5, 1, 1))
-plot(selprop_ranked,
-     type = "h", lwd = 3, las = 1, cex.lab = 1.3, bty = "n", ylim = c(0, 1),
-     col = ifelse(selprop_ranked >= Argmax(stab)[2], yes = batch.colours[m], no = "grey"),
-     xaxt = "n", xlab = "", ylab = "Selection proportions"
-)
-abline(h = Argmax(stab)[2], lty = 2, col = "darkred")
-axis(side = 1, at = 1:length(selprop_ranked), labels = NA)
-for (k in 1:length(selprop_ranked)){
-  if (beta_mu_ranked[k] > 0){
-    col = "red"
-  } else if (beta_mu_ranked[k] < 0){
-    col = "blue"
-  } else {
-    col = "black"
-  }
-  axis(side = 1, at = k, las = 2, labels = mylabels[k],
-       col.axis = ifelse(selprop_ranked[k] >= Argmax(stab)[2], col, "grey"))
-}
-dev.off()
-
-## Only exposures ----
-stab = VariableSelection(xdata = X[,(ncol(X)-ncol(delta_mat)+1):ncol(X)], ydata = Y, implementation = SparsePLS)
-pdf(paste0("../Figures/",filepaths[m],"/Shortest_path_multivariate_exposures_only_output.pdf"))
-par(mar = c(7, 5, 7, 6))
-CalibrationPlot(stab)
-dev.off()
-
-# Checking consistency in sign of the beta coefficients for the variables with high selprop
-# Ideally no point around 0.5 with high selection proportion
-selprop=SelectionProportions(stab)
-a=apply(stab$Beta[ArgmaxId(stab)[1],,],1,FUN=function(x){sum(x>0)})
-a = a[-length(a)]
-b=apply(stab$Beta[ArgmaxId(stab)[1],,],1,FUN=function(x){sum(x<0)})
-b = b[-length(b)]
-pdf(paste0("../Figures/",filepaths[m],"/Shortest_path_multivariate_exposures_only_beta_consistency.pdf"))
-par(mar=c(5,5,1,1))
-plot(a/(a+b), selprop, las=1, pch=19, col="navy", cex.lab=1.5,
-     xlab="Proportion of positive beta among non-zero betas", 
-     ylab="Selection Proportion") # Ideally no point around 0.5 with high selection proportion
-dev.off()
-
-# Extract beta
-beta = stab$Beta[ArgmaxId(stab)[1],,]
-beta_mu = rowMeans(beta[-nrow(beta),] %*% diag(beta[nrow(beta),]))
-names(beta_mu) = names(selprop)
-
-selprop_ranked <- sort(selprop, decreasing = TRUE)
-beta_mu_ranked <- beta_mu[order(-selprop)]
-print(all(names(selprop_ranked)==names(beta_mu_ranked)))
-
-mylabels = gsub("`","",names(selprop_ranked))
-
-pdf(paste0("../Figures/",filepaths[m],"/Shortest_path_multivariate_exposures_only_selprop.pdf"), width = 14)
-par(mar=c(15, 5, 1, 1))
-plot(selprop_ranked,
-     type = "h", lwd = 3, las = 1, cex.lab = 1.3, bty = "n", ylim = c(0, 1),
-     col = ifelse(selprop_ranked >= Argmax(stab)[2], yes = batch.colours[m], no = "grey"),
-     xaxt = "n", xlab = "", ylab = "Selection proportions"
-)
-abline(h = Argmax(stab)[2], lty = 2, col = "darkred")
-axis(side = 1, at = 1:length(selprop_ranked), labels = NA)
-for (k in 1:length(selprop_ranked)){
-  if (beta_mu_ranked[k] > 0){
-    col = "red"
-  } else if (beta_mu_ranked[k] < 0){
-    col = "blue"
-  } else {
-    col = "black"
-  }
-  axis(side = 1, at = k, las = 2, labels = substitute(Delta~tmp, list(tmp=mylabels[k])),
-       col.axis = ifelse(selprop_ranked[k] >= Argmax(stab)[2], col, "grey"))
-}
-dev.off()
-
-### Geographical ----
-# Load data
-expo = readRDS(paste0("../Processed/",filepaths[m],"/Exposure_matrix_ndimp_thresh_log_naimp_no_isolated.rds"))
-covars = readRDS(paste0("../Processed/",filepaths[m],"/Participant_covariate_info_thresh_no_isolated.rds"))
-print(all(rownames(expo)==rownames(covars)))
-
-families=unique(covars$Batch)
-
-# mycolours=brewer.pal(n=12,name='Paired')
-# mycolours=colorRampPalette(mycolours)(length(families))
-# names(mycolours)=families
-
-expo = scale(expo)
-d=dist(expo)
-h=hclust(d, method = "complete")
-print(all(covars$Indiv.ID==h$labels))
-h$labels=paste0(covars$Family.ID, "-",h$labels)
-myphylo=as.phylo(h)
-
-{pdf(paste0("../Figures/",filepaths[m],"/Hierarchical_cont_expo_all_batch.pdf"),width=14)
-  par(mar=c(0,0,0,0))
-  plot(myphylo, direction="downwards", cex=0.5, #srt=180, adj=1,
-       tip.color=batch.colours[as.character(covars$Batch)])
-  dev.off()
-}
-{pdf(paste0("../Figures/",filepaths[m],"/Hierarchical_cont_graph_expo_all_batch.pdf"))
-  g=ClusteringToGraph(covars=covars, myphylo=myphylo, mycol = batch.colours[as.character(covars$Batch)])
-  dev.off()
-}
-if (m == 4){
-  families=unique(covars$Region)
-  mycolours = region.colours
-  names(mycolours)=families
-  
-  {pdf(paste0("../Figures/",filepaths[m],"/Hierarchical_cont_expo_all_region.pdf"),width=14)
-    par(mar=c(0,0,0,0))
-    plot(myphylo, direction="downwards", cex=0.5, #srt=180, adj=1,
-         tip.color=region.colours[as.character(covars$Region)])
-    dev.off()
-  }
-  
-  {pdf(paste0("../Figures/",filepaths[m],"/Hierarchical_cont_graph_expo_all_region.pdf"))
-    g=ClusteringToGraph(covars=covars, myphylo=myphylo, mycol = region.colours[as.character(covars$Region)])
-    legend("bottomright", pch=19, col=region.colours[levels(covars$Region)],
-           legend=levels(covars$Region), cex = 0.4, ncol = 1)
-    dev.off()
-  }
-  
-  {pdf(paste0("../Figures/",filepaths[m],"/Hierarchical_cont_expo_all_depart.pdf"),width=14)
-    par(mar=c(0,0,0,0))
-    plot(myphylo, direction="downwards", cex=0.5, #srt=180, adj=1,
-         tip.color=depart.colours[as.character(covars$Department)])
-    dev.off()
-  }
-  
-  {pdf(paste0("../Figures/",filepaths[m],"/Hierarchical_cont_graph_expo_all_depart.pdf"))
-    g=ClusteringToGraph(covars=covars, myphylo=myphylo, mycol = depart.colours[as.character(covars$Department)])
-    legend("bottomright", pch=19, col=depart.colours[levels(covars$Department)],
-           legend=levels(covars$Department), cex = 0.4, ncol = 1)
-    dev.off()
-  }
-}
